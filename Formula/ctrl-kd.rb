5933d361b119096c439cf839dbe385320ab38b97
release-automation <release-automation@users.noreply.github.com>
2026-09-17 16:49:34 +0000

    ctrl-kd 4.10.0 (automated)

--- Formula/ctrl-kd.rb
+++ Formula/ctrl-kd.rb
@@ -3,8 +3,8 @@ class CtrlKd < Formula
 
   desc "Convert WordStar 4-7 documents and print-to-disk files to text, Markdown, HTML, RTF, or PDF"
   homepage "https://github.com/jonmichaels/ctrl-kd"
-  url "https://files.pythonhosted.org/packages/25/7f/b3a0edd07cd8a1b269789f0afad4939aaca85a818a026c902ab41666db07/ctrl_kd-4.9.0.tar.gz"
-  sha256 "1f77694622cf4fa6451ee669ac0ddb4822a5702c7cf6a9c17de23902e12d4aef"
+  url "https://files.pythonhosted.org/packages/eb/81/f1d905f753f4767423eb161f70a383b3388e15978ea3e9fe550296fb0524/ctrl_kd-4.10.0.tar.gz"
+  sha256 "7ad8ca57f642aaaa95e244ee4fd4c64a8267e6196ef5e7d5c92f34639a22a93d"
   license "MIT"
 
   depends_on "python@3.12"
